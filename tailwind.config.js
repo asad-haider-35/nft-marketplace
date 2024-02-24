b645550b2dbf688c6a2868a3cc3b159cf3a15f1c
Asad Haider <asad.haider@techlogix.com>
2024-02-24 12:37:47 +0500

    Installed & Configured Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#031333',
+        secondary: '#67F8AE',
+        dimWhite: '#C8C8C8',
+      },
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"]
+      }
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px"
+    }
+  },
+  plugins: [],
+}
+
