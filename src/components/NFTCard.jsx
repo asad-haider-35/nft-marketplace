9458dae4c7e6e3b001e1cd7ac95daf963a06a29d
Sarmad Hussain <sarmadhusssain@gmail.com>
2024-03-11 00:40:50 +0500

    NFT Card design updated
    Removed unnecessary code and added new things

--- src/components/NFTCard.jsx
+++ src/components/NFTCard.jsx
@@ -3,39 +3,40 @@ import { SquidO } from "../assets";
 
 const NFTCard = ({ nft }) => {
   return (
-    <div
-      className="rounded-lg shadow-md bg-primary h-96 w-96 flex flex-row justify-center items-center rounded-3xl"
-    >
-      <div  
-        className="bg-center bg-contain bg-dimBlue h-5/6 w-5/6 rounded-2xl" 
-        style={{ backgroundImage: `url(${nft.imagePath})` }}>
-        
-      </div>
-      {/* <div className="relative">
-        <img className="w-full h-full object-cover rounded-t-lg" src={nft.imagePath} alt={nft.name} />
-        <div className="absolute inset-0 bg-gradient-to-r from-gray-800/50 to-transparent"></div>
-        <div className="absolute inset-x-0 bottom-0 px-6 py-4 flex justify-between items-center">
-          <p className="text-white text-lg font-bold">{nft.name}</p>
-          <p className="text-white text-sm">{nft.code}</p>
-        </div>
-      </div>
-      <div className="p-6 flex flex-col justify-between">
-        <div>
-          <p className="text-gray-700">Remaining Time</p>
-          <p className="font-bold text-lg text-gray-800">{nft.remainingTime}</p>
-        </div>
-        <div className="mt-4 flex items-center justify-between">
-          <div>
-            <p className="text-gray-700">Current Bid</p>
-            <p className="font-bold text-lg text-gray-800">{nft.currentBidInETH}</p>
+    <div className="rounded-lg shadow-md bg-primary h-96 w-96 flex flex-row justify-center items-center rounded-3xl">
+      <div
+        className="relative  bg-center bg-contain bg-dimBlue h-[22rem] w-[22rem] rounded-2xl"
+        style={{ backgroundImage: `url(${nft.imagePath})` }}
+      >
+        <div className="absolute top-0 right-0">
+          <div className="bg-primary w-32">
+            <p className="text-white text-xs">Remaining Time</p>
+            <p className="font-bold text-md text-white ">07H : 10M : 30S</p>
           </div>
-          <button
-            className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700"
-          >
-            Place Bid
-          </button>
         </div>
-      </div> */}
+
+        <div className="absolute inset-y-0 left-0 flex items-center">
+          <div className="bg-primary rounded-r-lg h-44 w-7">
+            <p className="text-white text-xs [writing-mode:vertical-lr]">
+              # {nft.code} {nft.name}
+            </p>
+          </div>
+        </div>
+
+        <div className="absolute inset-x-0 bottom-0">
+          <div className="grid gap-2 grid-cols-2">
+            <div className="bg-primary">
+              <p className="text-white ">Current Bid</p>
+              <p className="font-bold text-lg text-white">
+                {nft.currentBidInETH}
+              </p>
+            </div>
+            <button className="px-4 py-2 gradient text-white text-white rounded-md">
+              Place Bid
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
