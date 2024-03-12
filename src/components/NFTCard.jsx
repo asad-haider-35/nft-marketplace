f1508a97b5ffbdb58b8926adced5fc173ed7927b
Sarmad Hussain <sarmadhusssain@gmail.com>
2024-03-13 00:57:06 +0500

    NFT Card UI Updated

--- src/components/NFTCard.jsx
+++ src/components/NFTCard.jsx
@@ -9,31 +9,33 @@ const NFTCard = ({ nft }) => {
         style={{ backgroundImage: `url(${nft.imagePath})` }}
       >
         <div className="absolute top-0 right-0">
-          <div className="bg-primary w-32">
+          <div className="bg-primary w-32 rounded-lg">
             <p className="text-white text-xs">Remaining Time</p>
-            <p className="font-bold text-md text-white ">07H : 10M : 30S</p>
+            <p className="font-bold text-md text-secondary ">07H : 10M : 30S</p>
           </div>
         </div>
 
         <div className="absolute inset-y-0 left-0 flex items-center">
-          <div className="bg-primary rounded-r-lg h-44 w-7">
+          <div className="bg-primary rounded-r-lg h-44 w-7 flex items-center">
             <p className="text-white text-xs [writing-mode:vertical-lr]">
               # {nft.code} {nft.name}
             </p>
           </div>
         </div>
 
-        <div className="absolute inset-x-0 bottom-0">
-          <div className="grid gap-2 grid-cols-2">
-            <div className="bg-primary">
-              <p className="text-white ">Current Bid</p>
-              <p className="font-bold text-lg text-white">
-                {nft.currentBidInETH}
-              </p>
+        <div className="absolute inset-x-0 bottom-0 h-16">
+          <div className="grid gap-2 grid-cols-2 h-16">
+            <div className="bg-primary rounded-r-lg transform skew-x-6">
+              <div className="transform -skew-x-6 flex flex-col items-start pl-4 pt-2">
+                <p className="text-white text-xs">Current Bid</p>
+                <p className="font-bold text-xl text-secondary">
+                  {nft.currentBidInETH}
+                </p>
+              </div>
             </div>
-            <button className="px-4 py-2 gradient text-white text-white rounded-md">
-              Place Bid
-            </button>
+            <div className="px-4 py-2 gradient text-white text-white rounded-md transform skew-x-6 flex items-center mb-2">
+              <div className="transform -skew-x-6 pl-8 ">Place Bid</div>
+            </div>
           </div>
         </div>
       </div>
